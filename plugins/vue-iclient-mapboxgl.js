707d4b7372e4c60ae82c81de7dc8ef8102fb455f
MoY8462 <moy.sav21@hotmail.com>
2023-08-25 12:21:07 -0600

    feat: :sparkles: initialize proyect

---
+++ plugins/vue-iclient-mapboxgl.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueiClient from '@supermap/vue-iclient-mapboxgl'
+
+const white = '#FFFFFF'
+const primary = '#0C6170'
+const secondary = '#37BEB0'
+const tertiary = '#A4E5E0'
+const quaternary = '#DBF5F0'
+const quinary = '#C3CEDA'
+
+let theme = {
+  textColor: primary,
+  background: quinary,
+  componentBackground: white,
+  clickColor: secondary,
+  hoverColor: secondary,
+  colorGroup: [primary, secondary, tertiary, quaternary, quinary],
+}
+
+Vue.use(VueiClient, { theme })
